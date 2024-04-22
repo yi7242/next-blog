ce23d54b34a2afeff064a3e0389861b7eeff9556
Your Name <47847960+yi7242@users.noreply.github.com>
2024-04-23 00:11:47 +0900

    現状

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,5 @@
+import Navbar from '@/components/navbar/Navbar'
+import Footer from '@/components/footer/Footer'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -11,7 +13,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className='container'>
+          <div className='wrapper'>
+            <Navbar />
+            {children}
+            <Footer />
+          </div>
+        </div>
+      </body>
     </html>
   )
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,9 @@
+import Navbar from "@/components/navbar/Navbar";
 import styles from "./homepage.module.css";
 import Link from "next/link";
+import Footer from "@/components/footer/Footer";
 export default function Home() {
-  return <div><Link href="/">test</Link></div>;
+  return <div>
+
+  </div>
 }
