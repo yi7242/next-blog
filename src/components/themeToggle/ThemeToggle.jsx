f11c1294337134a3d7d62857d11faacebb00e0f5
Your Name <47847960+yi7242@users.noreply.github.com>
2024-06-23 20:10:09 +0900

    hamburger menu

--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -9,9 +9,9 @@ const ThemeToggle = () => {
     const { theme, toggle } = useContext(ThemeContext)
     console.log(theme)
     return (
-        <div className={styles.container} onClick={toggle}>
+        <div className={styles.container} style={theme === "dark" ? { background: "white" } : { background: "#0f172a" }} onClick={toggle}>
             <Image src="/moon.png" alt="moon" width={14} height={14} />
-            <div className={styles.ball}></div>
+            <div className={styles.ball} style={theme === "dark" ? { left: 1, background: "#0f172a" } : { right: 1, background: "white" }}></div>
             <Image src="/sun.png" alt="sun" width={14} height={14} />
         </div>
     )
