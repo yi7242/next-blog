4490a0c276e922f531e428154c2987f080decece
Your Name <47847960+yi7242@users.noreply.github.com>
2024-06-23 23:29:34 +0900

    featured title

--- src/components/Featured/Featured.jsx
+++ src/components/Featured/Featured.jsx
@@ -3,7 +3,9 @@ import styles from "./featured.module.css";
 
 const Featured = () => {
     return (
-        <div className={styles.container}>Featured</div>
+        <div className={styles.container}>
+            <h1 className={styles.title}><b>Hello everyone, Yunative here!</b> Discover my ideas.</h1>
+        </div>
     )
 }
 
