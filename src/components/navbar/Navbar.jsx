13e14e8bb85adb782830e12c25a127a8a302cca7
Your Name <47847960+yi7242@users.noreply.github.com>
2024-06-23 18:05:09 +0900

    add icon

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import Image from 'next/image';
 import styles from "./navbar.module.css";
 
 const Navbar = () => {
+    https://score.wanictf.org/#/login
     return (
         <div className={styles.container}>
             <div className={styles.social}>
-                <Image></Image>
+                <Image src="/facebook.png" alt='facebook' width={24} height={24} />
+                <Image src="/instagram.png" alt='instagram' width={24} height={24} />
+                <Image src="/tiktok.png" alt='tiktok' width={24} height={24} />
+                <Image src="/youtube.png" alt='youtube' width={24} height={24} />
             </div>
             <div className={styles.logo}></div>
             <div className={styles.links}></div>
