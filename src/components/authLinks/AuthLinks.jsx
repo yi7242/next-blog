98e716b444a3f4d48d163e956495ca3a0ee95a29
Your Name <47847960+yi7242@users.noreply.github.com>
2024-06-23 18:24:46 +0900

    authLinks

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -1,10 +1,24 @@
+import Link from "next/link";
 import styles from "./authLinks.module.css";
 import React from 'react'
 
 const AuthLinks = () => {
+    // temporary
+    const status = "authenticated"
     return (
-        <div className={styles.container}>AuthLinks</div>
-    )
+        <>
+            {
+                status === "notauthenticated" ? (
+                    <Link href="/login">Login</Link>
+                ) : (
+                    <>
+                        <Link href="/write">Write</Link>
+                        <span className={styles.link}>Logout</span>
+                    </>
+                )
+            }
+        </>
+    );
 }
 
 export default AuthLinks
