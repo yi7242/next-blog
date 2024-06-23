d04e68cd0472628e09f18521852256dd2bbfb6b2
Your Name <47847960+yi7242@users.noreply.github.com>
2024-06-24 01:04:32 +0900

    add top page

--- src/components/Featured/Featured.jsx
+++ src/components/Featured/Featured.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import styles from "./featured.module.css";
+import Image from 'next/image';
 
 const Featured = () => {
     return (
         <div className={styles.container}>
-            <h1 className={styles.title}><b>Hello everyone, Yunative here!</b> Discover my ideas.</h1>
+            <h1 className={styles.title}><b>Hello, Yunative here!</b> Discover my ideas.</h1>
+            <div className={styles.post}>
+                <div className={styles.imgContainer}>
+                    <Image src="/p1.jpeg" alt="" fill />
+                </div>
+                <div className={styles.textContainer}>
+                    <h1 className={styles.postDesc}>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</h1>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui ipsum minus cupiditate atque, totam, adipisci quam ducimus a quasi similique, delectus ab possimus molestias iste? Sit iure optio ea amet?</p>
+                    <button className={styles.button}>Read More</button>
+                </div>
+            </div>
         </div>
     )
 }
