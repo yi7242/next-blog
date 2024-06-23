bb1d0aa9bb3df5604c8450f389b95b8b4156e930
Your Name <47847960+yi7242@users.noreply.github.com>
2024-06-24 01:10:46 +0900

    feat: Add new keyboard shortcuts and improve CSS styling in memo.txt and Featured component

--- src/components/Featured/Featured.jsx
+++ src/components/Featured/Featured.jsx
@@ -8,7 +8,7 @@ const Featured = () => {
             <h1 className={styles.title}><b>Hello, Yunative here!</b> Discover my ideas.</h1>
             <div className={styles.post}>
                 <div className={styles.imgContainer}>
-                    <Image src="/p1.jpeg" alt="" fill />
+                    <Image src="/p1.jpeg" alt="" fill className={styles.image} />
                 </div>
                 <div className={styles.textContainer}>
                     <h1 className={styles.postDesc}>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</h1>
