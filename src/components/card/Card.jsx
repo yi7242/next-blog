f7f15d8190e71336f178f9c697405e01b9b6a478
yi7242 <47847960+yi7242@users.noreply.github.com>
2024-06-24 10:11:51 +0900

    fix bug

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,25 +1,32 @@
-import Image from 'next/image'
-import styles from './card.module.css'
-import React from 'react'
-import Link from 'next/link'
+import Image from "next/image";
+import styles from "./card.module.css";
+import React from "react";
+import Link from "next/link";
 
 const Card = () => {
-    return (
-        <div className={styles.imagecontainer}>
-            <Image src="/p1.jpeg" alt="" fill className={styles.image} />
-            <div className={styles.textContainer}>
-                <div className={styles.detail}>
-                    <span className={styles.date}>2024.06.24</span>
-                    <span className={styles.category}>CULTURE</span>
-                </div>
-                <Link href="/">
-                    <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</h1>
-                </Link>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores, natus voluptates. Repudiandae numquam libero sunt. Porro minima officiis distinctio adipisci, illum impedit praesentium est officia. Veniam fugiat eum aut iusto!</p>
-                <Link href="/">Read More</Link>
-            </div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.imagecontainer}>
+        <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+      </div>
+      <div className={styles.textContainer}>
+        <div className={styles.detail}>
+          <span className={styles.date}>2024.06.24</span>
+          <span className={styles.category}>CULTURE</span>
         </div>
-    )
-}
+        <Link href="/">
+          <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</h1>
+        </Link>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores,
+          natus voluptates. Repudiandae numquam libero sunt. Porro minima
+          officiis distinctio adipisci, illum impedit praesentium est officia.
+          Veniam fugiat eum aut iusto!
+        </p>
+        <Link href="/">Read More</Link>
+      </div>
+    </div>
+  );
+};
 
-export default Card
+export default Card;
