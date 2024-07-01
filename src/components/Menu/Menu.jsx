b519cb2cee27d1ebfbf6bb99ac0b420d400c5580
Your Name <47847960+yi7242@users.noreply.github.com>
2024-07-01 23:28:40 +0900

    1:26:33

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -15,6 +15,21 @@ export default function Menu() {
         <Link href="/blog?cat=style" className={`${styles.categoryItem} ${styles.style}`}>
           Style
         </Link>
+        <Link href="/blog?cat=fashion" className={`${styles.categoryItem} ${styles.fashion}`}>
+          Fashion
+        </Link>
+        <Link href="/blog?cat=food" className={`${styles.categoryItem} ${styles.food}`}>
+          Food
+        </Link>
+        <Link href="/blog?cat=travel" className={`${styles.categoryItem} ${styles.travel}`}>
+          Travel
+        </Link>
+        <Link href="/blog?cat=culture" className={`${styles.categoryItem} ${styles.culture}`}>
+          Culture
+        </Link>
+        <Link href="/blog?cat=coding" className={`${styles.categoryItem} ${styles.coding}`}>
+          Coding
+        </Link>
       </div>
       <h2 className={styles.subtitle}>{"Chosen by the editor"}</h2>
       <h1 className={styles.title}>Editors Pick</h1>
